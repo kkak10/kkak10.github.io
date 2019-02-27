25ca10984c9cc1f5d47a52e2b72c8ae274732f89
leehanur <kkak1072@gmail.com>
2019-02-27 16:43:24 +0900

    Test용 App으로 변경

---
+++ precache-manifest.894bc11967a0b0c4bf40806a6ce6acff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b6df53cd46ce11ea1a6c",
+    "url": "/static/js/main.b6df53cd.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b6df53cd46ce11ea1a6c",
+    "url": "/static/css/main.196f35cf.chunk.css"
+  },
+  {
+    "revision": "1f134ca8ba6fd292d9b2cf36711858c7",
+    "url": "/index.html"
+  }
+];
